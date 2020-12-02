7d2727c292ac03f7ecaf454299bcecbad345f776
Denis Popkov <pkvdenis@gmail.com>
2020-12-02 15:21:40 +0300

    leetcode: p0003 Longest Substring Without Repeating Characters: add optimized.

--- leetcode/src/main/java/learn/algo/leetcode/problemset/p0003longsubstring/Solution.java
+++ leetcode/src/main/java/learn/algo/leetcode/problemset/p0003longsubstring/Solution.java
@@ -1,8 +1,13 @@
 package learn.algo.leetcode.problemset.p0003longsubstring;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Solution {
+
+    /* My Solution. It may be improved. */
     /** Finds  length of the longest substring without repeating characters. */
     public int lengthOfLongestSubstring(String s) {
         // Map содержит символ и индекс обнаружения символа
@@ -28,6 +33,52 @@ public class Solution {
             map.put(ch, i);
             int length = i - start + 1;
             longest = Math.max(length, longest);
+        }
+        return longest;
+    }
+
+    /*
+    Sliding Window Solution
+    -----------------------
+    Runtime: 6 ms, faster than 65.34%.
+    Memory Usage: 39.6 MB, less than 26.88%.
+     */
+    public int lengthOfLongestSubstringWithSlidingWindow(String s) {
+        int length = s.length();
+        Set<Character> set = new HashSet<>();
+        int longest = 0;
+        int i = 0;
+        int j = 0;
+        while (i < length && j < length) {
+            char ch = s.charAt(j);
+            if (!set.contains(ch)) {
+                set.add(ch);
+                j++;
+                longest = Math.max(longest, j - i);
+            } else {
+                set.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return longest;
+    }
+
+    /*
+    Sliding Window Optimized Solution
+    ---------------------------------
+    Runtime: 4 ms, faster than 89.65%.
+    Memory Usage: 39.1 MB, less than 61.21%
+     */
+    public int lengthOfLongestSubstringWithSlidingWindowOptimized(String s) {
+        int length = s.length();
+        int longest = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int start = 0, last = 0; last < length; last++) {
+            if (map.containsKey(s.charAt(last))) {
+                start = Math.max(map.get(s.charAt(last)), start);
+            }
+            longest = Math.max(longest, last - start + 1);
+            map.put(s.charAt(last), last + 1);
         }
         return longest;
     }
--- leetcode/src/test/java/learn/algo/leetcode/problemset/p0003longsubstring/SolutionTest.java
+++ leetcode/src/test/java/learn/algo/leetcode/problemset/p0003longsubstring/SolutionTest.java
@@ -1,33 +1,47 @@
 package learn.algo.leetcode.problemset.p0003longsubstring;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SuppressWarnings("SpellCheckingInspection")
 class SolutionTest {
+    private static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(0, ""),
+                Arguments.of(3, "abcabcbb"),
+                Arguments.of(1, "bbbbb"),
+                Arguments.of(3, "pwwkew"),
+                Arguments.of(2, "aab"),
+                Arguments.of(3, "dvdf"),
+                Arguments.of(6, "dabdefg"),
+                Arguments.of(5, "tmmzuxt"),
+                Arguments.of(6, "wobgrovw")
+        );
+    }
 
     @ParameterizedTest
-    @CsvSource({
-            "3, abcabcbb",
-            "1, bbbbb",
-            "3, pwwkew",
-            "2, aab",
-            "3, dvdf",
-            "6, dabdefg",
-            "5, tmmzuxt",
-            "6, wobgrovw"
-    })
+    @MethodSource("generateData")
     void testLengthOfLongestSubstringOk(int expected, String input) {
         int result = new Solution().lengthOfLongestSubstring(input);
         assertEquals(expected, result);
     }
 
-    @Test
-    void testLengthOfLongestSubstringZero() {
-        int result = new Solution().lengthOfLongestSubstring("");
-        assertEquals(0, result);
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void testLengthOfLongestSubstringWithSlidingWindowOk(int expected, String input) {
+        int result = new Solution().lengthOfLongestSubstringWithSlidingWindow(input);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void testLengthOfLongestSubstringWithSlidingWindowOptimizedOk(int expected, String input) {
+        int result = new Solution().lengthOfLongestSubstringWithSlidingWindowOptimized(input);
+        assertEquals(expected, result);
     }
 }
